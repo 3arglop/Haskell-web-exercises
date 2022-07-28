724242d1fc7a112b73a03333e1788c7609dca012
Estefania Argueta <estefania.arguetalopez13@gmail.com>
2022-07-28 14:42:38 -0500

    EXERCISE

--- js/external.js
+++ js/external.js
@@ -7,6 +7,7 @@ console.log(color);
 
 alert("Great, blue is my favorite color too\!");
 
+//1.
 var mermaid = 3;
 var bear = 5;
 var hercules = 1;
@@ -17,4 +18,28 @@ var bT = bear * price;
 var hT = hercules * price;
 
 var total = mT + bT + hT;
+
+//2.
+
+var google = 400;
+var amazon = 380;
+var facebook = 350;
+
+var gTotal = google * 6;
+var aTotal = amazon * 4;
+var fTotal = facebook * 10;
+
+var paycheck = gTotal + fTotal + aTotal;
+
+//3.
+
+var isFull = false;
+var isConflict = false;
+let enroll = (!isFull && !isConflict);
+
+//4.
+
+var product;
+var offer = product > 2;
+var premium = false;
 
