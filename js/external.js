23ad63c4e2ec9c696c2632357e71b472e9773dd8
Estefania Argueta <estefania.arguetalopez13@gmail.com>
2022-07-28 14:02:45 -0500

    JS+HTML

--- js/external.js
+++ js/external.js
@@ -1,1 +1,11 @@
-console.log("THIS IS MY EXTERNAL FILE");
+"use strict";
+
+alert("Welcome to my Website\!");
+
+var color = prompt("What is your favorite color\?");
+console.log(color);
+
+alert("Great, blue is my favorite color too\!");
+
+
+
