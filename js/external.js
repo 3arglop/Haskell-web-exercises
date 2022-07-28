c1499699b2c5e175672680e3b082dc0cc43e812b
Estefania Argueta <estefania.arguetalopez13@gmail.com>
2022-07-28 14:40:02 -0500

    EXERCISE

--- js/external.js
+++ js/external.js
@@ -7,5 +7,14 @@ console.log(color);
 
 alert("Great, blue is my favorite color too\!");
 
+var mermaid = 3;
+var bear = 5;
+var hercules = 1;
+var price = 3;
 
+var mT = mermaid * price;
+var bT = bear * price;
+var hT = hercules * price;
+
+var total = mT + bT + hT;
 
