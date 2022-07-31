abc42fb723fb6b3c647f7546f39b1aad0583d1fa
Estefania Argueta <estefania.arguetalopez13@gmail.com>
2022-07-31 13:36:42 -0500

    EXERCISE

--- js/external.js
+++ js/external.js
@@ -8,38 +8,55 @@ console.log(color);
 alert("Great, blue is my favorite color too\!");
 
 //1.
-var mermaid = 3;
-var bear = 5;
-var hercules = 1;
-var price = 3;
+var mermaid = prompt("How many days is The Little Mermaid being rented for\?");
+console.log(mermaid);
 
-var mT = mermaid * price;
-var bT = bear * price;
-var hT = hercules * price;
+var bear = prompt("How many days is Brother Bear being rented for\?");
+console.log(bear);
 
-var total = mT + bT + hT;
+var hercules = prompt("How many days is Hercules being rented for\?");
+console.log(hercules);
+
+var mT = mermaid * 3;
+var bT = bear * 3;
+var hT = hercules * 3;
+
+alert("Your grand total is " + "$" + (mT + bT + hT) + ".");
 
 //2.
-
 var google = 400;
 var amazon = 380;
 var facebook = 350;
 
-var gTotal = google * 6;
-var aTotal = amazon * 4;
-var fTotal = facebook * 10;
+var gHours = prompt("How many hours did you work at Google\?");
+var aHours = prompt("How many hours did you work at Amazon\?");
+var fHours = prompt("How many hours did you work at Facebook\?");
 
-var paycheck = gTotal + fTotal + aTotal;
+var paycheck = (google * gHours) + (amazon * aHours) + (facebook * fHours);
+
+alert("You will receive " + "$" + (paycheck) + " in your next paycheck\!");
 
 //3.
-
-var isFull = false;
-var isConflict = false;
+var isFull = confirm("Is this class fully enrolled\?");
+var isConflict = confirm("Will this class conflict with your schedule\?");
 let enroll = (!isFull && !isConflict);
+alert("Enrollment Status: " + enroll);
 
 //4.
+var member = confirm("Are you a Premium member\?"); // true for OK false for CANCEL
+var limit = !member;
+alert("Minimum purchase of 2 required for this offer: " + limit);
+prompt("Quantity:");
 
-var product;
-var offer = product > 2;
-var premium = false;
+var expiration = confirm("Has the offer expired\?"); // true for OK false for CANCEL
+alert("This offer can be applied: " + !expiration);
+alert("Thank you for your purchase\!");
+
+
+
+
+
+
+
+
 
