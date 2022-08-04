0d0d14717823120c1d2c9902bb08879c44f379ee
Estefania Argueta <estefania.arguetalopez13@gmail.com>
2022-08-04 11:02:46 -0500

    EXERCISE

--- js/planets-array.js
+++ js/planets-array.js
@@ -20,23 +20,31 @@ console.log("Time to seek some planets!");
      * the step that it describes
      */
 
+    planets.unshift('The Sun');
     console.log('Adding "The Sun" to the beginning of the planets array.');
     console.log(planets);
 
+    planets.push('Pluto');
     console.log('Adding "Pluto" to the end of the planets array.');
     console.log(planets);
 
+    planets.shift('The Sun');
     console.log('Removing "The Sun" from the beginning of the planets array.');
     console.log(planets);
 
+    planets.pop('Pluto');
     console.log('Removing "Pluto" from the end of the planets array.');
     console.log(planets);
 
+    var index = planets.indexOf('Earth');
     console.log('Finding and logging the index of "Earth" in the planets array.');
+    console.log(index);
 
+    planets.reverse();
     console.log("Reversing the order of the planets array.");
     console.log(planets);
 
+    planets.sort()
     console.log("Sorting the planets array.");
     console.log(planets);
 })();
