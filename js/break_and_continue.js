8fc279bb46236e81e2d8f8aae62bf8d47fee180f
Estefania Argueta <estefania.arguetalopez13@gmail.com>
2022-08-05 14:47:03 -0500

    EXERCISE

---
+++ js/break_and_continue.js
@@ -0,0 +1,16 @@
+//BREAK AND CONTINUE
+
+//EXERCISE 1
+
+console.log("Number to skip is: 27");
+
+var twentySeven;
+for(twentySeven = 1; twentySeven <= 50; twentySeven += 2) {
+    console.log(twentySeven);
+    if(twentySeven === 27) {
+        console.log("Yikes! Skipping number: ")
+        continue;
+    }
+    console.log("Here is an odd number: ")
+        }
+
