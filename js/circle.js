a06b4b28e0684df844ead205e1c409d4944feb9b
Estefania Argueta <estefania.arguetalopez13@gmail.com>
2022-08-08 16:23:50 -0500

    EXERCISE

--- js/circle.js
+++ js/circle.js
@@ -2,24 +2,31 @@
     console.log("I LOVE MATH!!!!!");
     "use strict";
 
+    // Returns a random integer between min and max.
+    //function getRandomInt(min, max) {
+      //  return Math.floor(Math.random() * (max - min + 1) + min);
+    //}
+    //console.log(getRandomInt(10, 1000));
+
     // create a circle object
     var circle = {
         radius: 3,
 
-        getArea: function(circle) {
+        getArea: function() {
             // TODO: complete this method
-            // hint: area = pi * radius^2
-
-            return; // TODO: return the proper value
+            var area = Math.PI * (Math.pow(circle.radius, 2));
+            return area;
         },
-
         logInfo: function (doRounding) {
             // TODO: complete this method.
-
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
-
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            if(doRounding === true) {
+               var rounded = Math.round(circle.getArea());
+                console.log("Area of a circle with radius: " + circle.radius + ", is: " + rounded);
+            } else {
+                console.log("Area of a circle with radius: " + circle.radius + ", is: " + circle.getArea());
+            }
         }
     };
 
@@ -31,7 +38,7 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+    circle.radius = 5;
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
