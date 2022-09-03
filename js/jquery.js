635e0d89abdb36fcbd693c669ba41fd71e483e7e
3arglop <estefania.arguetalopez13@gmail.com>
2022-09-03 11:32:40 -0500

    jQuery: SELECTORS

--- js/jquery.js
+++ js/jquery.js
@@ -1,3 +1,22 @@
-$(function() {
-    alert( 'The DOM has finished loading!' );
-});
+// $(function() {
+//     alert( 'Welcome to the world of jQuery!' );
+// });
+
+var greetings = $('#header').html();
+alert(greetings);
+
+// var facts = $('#title').html();
+// alert(facts);
+
+// $('.codeup').css('border', '1px solid red');
+
+// $('li').css('font-size', '20px');
+//
+// $('li').css('background-color', 'green');
+//
+// $('#header').css('background-color', 'red');
+//
+// $('.para').css('background-color', 'blue');
+
+$('#header, .para, li').css('background-color', 'pink');
+
