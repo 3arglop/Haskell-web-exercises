8f0554744790879c8d17e5329460215b5fbf2694
3arglop <estefania.arguetalopez13@gmail.com>
2022-09-06 12:01:11 -0500

    jQuery: MOUSE EVENTS ON PREVIOUS HTML FILE ADDED

--- js/jquery.js
+++ js/jquery.js
@@ -2,8 +2,8 @@
 //     alert( 'Welcome to the world of jQuery!' );
 // });
 
-var greetings = $('#header').html();
-alert(greetings);
+// var greetings = $('#header').html();
+// alert(greetings);
 
 // var facts = $('#title').html();
 // alert(facts);
@@ -18,5 +18,28 @@ alert(greetings);
 //
 // $('.para').css('background-color', 'blue');
 
-$('#header, .para, li').css('background-color', 'pink');
+// $('#header, .para, li').css('background-color', 'pink');
 
+var header = $('#header');
+header.click(function(e) {
+    header.css('background', 'pink');
+    console.log("header clicked!");
+});
+
+var para =  $('p');
+para.dblclick(function(e) {
+    para.css('font-size', '18px');
+    console.log("p tags dbl clicked!");
+    console.log(e);
+});
+
+var listItems = $('li');
+listItems.hover(
+    function(e) {
+    listItems.css('background', 'purple');
+        console.log("Inside");
+    },
+    function(e) {
+    listItems.css('background', 'yellowgreen');
+        console.log("Outside");
+    });
