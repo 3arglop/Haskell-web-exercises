86e3b80290500cbae52c295a1b5f9c687ce73e37
3arglop <estefania.arguetalopez13@gmail.com>
2022-09-10 11:26:25 -0500

    AJAX STORE: updated

--- js/konami.js
+++ js/konami.js
@@ -30,8 +30,57 @@ $(window).ready(function() {
     });
 });
 
+var keysPressed = [];
+var konami = [38,38,40,40,37,39,37,39,66,65]; //NO 13 FOR ENTER
+let keyCount = 0;
+
+// var success = function() {
+//     if(input === 10) {
+//         alert("YAY! YOU DID IT!");
+//         console.log("full konami code entered");
+//     }
+// }
+//
+// var reset = function() {
+//     console.log("start again");
+//     keyCount;
+// }
 
 
+$(window).ready(function() {
+    body.keyup(function(e) {
+        console.log("keyup successful!");
+        console.log(e.keyCode);
+        var input = keysPressed.push(e.keyCode);
+        console.log(input);
+
+        if(input === konami[keyCount]) {
+            keyCount++;
+
+            if (input === konami.length) {
+                // success();
+                // reset();
+                alert("yay!");
+            } else {
+                alert("try again buckaroo");
+                // reset();
+            }
+        }
+
+
+
+        // for(let i = 0; i < konami.length; i += 1)
+        // if(input[i] === konami[i]) {
+        //     console.log("yay!");
+        //     input++;
+        // } else {
+        //     alert("try again!")
+        //     reset;
+        // }
+
+
+    });
+});
 
 
 // $(document).keyup(function(e) {
