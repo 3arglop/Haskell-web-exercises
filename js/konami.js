2875a9a32b2c6f563650c13a37775e828073cca4
3arglop <estefania.arguetalopez13@gmail.com>
2022-09-11 23:51:45 -0500

    KONAMI: still in progress...

--- js/konami.js
+++ js/konami.js
@@ -18,21 +18,29 @@ console.log(code.text());
 var body = $('.bodyElement');
 console.log(body.text());
 
-var showCode = code.addClass('show');
-console.log(showCode);
+
 
 $(window).ready(function() {
     intro.dblclick(function(e) {
        $(this).addClass('hidden');
        alert('Know the Konami Code? Press the keys on your keyboard to achieve 30 lives!');
         console.log("intro clicked");
-        showCode.toggleClass('show');
+
     });
 });
 
+//DECLARING MY VARIABLES
 var keysPressed = [];
+
 var konami = [38,38,40,40,37,39,37,39,66,65]; //NO 13 FOR ENTER
-let keyCount = 0;
+
+let keyCount= 0;
+
+// var secretCode = konami.length;
+// console.log(secretCode);
+//
+// let inputCode = input.length;
+// console.log(inputCode);
 
 // var success = function() {
 //     if(input === 10) {
@@ -50,23 +58,101 @@ let keyCount = 0;
 $(window).ready(function() {
     body.keyup(function(e) {
         console.log("keyup successful!");
-        console.log(e.keyCode);
-        var input = keysPressed.push(e.keyCode);
-        console.log(input);
+        // console.log(e.keyCode);
+        let user = e.keyCode;
+        console.log(user);
+        keysPressed.push(user);
+        console.log(keysPressed);
+        // let inputCode = input.push("keysPressed");
+        // console.log(inputCode);
 
-        if(input === konami[keyCount]) {
-            keyCount++;
 
-            if (input === konami.length) {
-                // success();
-                // reset();
-                alert("yay!");
-            } else {
-                alert("try again buckaroo");
-                // reset();
-            }
+
+        switch (user) {
+            case keysPressed[0] === konami[0]:
+                console.log("yay");
+                break;
+            case keysPressed[1] === konami[1]:
+                console.log("yay");
+                break;
+            case keysPressed[2] === konami[2]:
+                 console.log("yay");
+                 break;
+            case keysPressed[3] === konami[3]:
+                console.log("yay");
+                break;
+            case keysPressed[4] === konami[4]:
+                console.log("yay");
+                break;
+            case keysPressed[5] === konami[5]:
+                console.log("yay");
+                break;
+            case keysPressed[6] === konami[6]:
+                console.log("yay");
+                break;
+            case keysPressed[7] === konami[7]:
+                console.log("yay");
+                break;
+            case keysPressed[8] === konami[8]:
+                console.log("yay");
+                break;
+            case keysPressed[9] === konami[9]:
+                console.log("yay");
+                break;
+            case keysPressed[10] === konami[10]:
+                alert("YAY! YOU DID IT!");
+                console.log("yay");
+                keyCount = 0;
+                break;
         }
 
+
+
+
+
+   // for(let i = 0; i < konami.keyCode; i++) {
+   //     if(keysPressed[i] === konami[10]) {
+   //         console.log("yay")
+   //         i = 0;
+   //     } else {
+   //         console.log("nope");
+   //     }
+   // }
+
+        // if(keysPressed[0] === secretCode[0]) {
+        //     keyCount++;
+        // } if(keyCount === konami.length) {
+        //     alert("yay!");
+        //     keyCount = 0;
+        // } else {
+        //     alert("try again");
+        //     keyCount = 0;
+        // }
+
+        // var checkCode = function() {
+        //     if(JSON.stringify(keysPressed) === JSON.stringify(konami)) {
+        //         alert("yay");
+        //     } else {
+        //         alert("try again!");
+        //         console.log("error");
+        //     }
+        //
+        // }
+        // checkCode();
+
+
+        // if(input === konami[keyCount]) {
+        //     keyCount++;
+        //
+        //     if (input === konami.length) {
+        //         // success();
+        //         // reset();
+        //         alert("yay!");
+        //     } else {
+        //         alert("try again buckaroo");
+        //         // reset();
+        //     }
+        // }
 
 
         // for(let i = 0; i < konami.length; i += 1)
@@ -81,32 +167,3 @@ $(window).ready(function() {
 
     });
 });
-
-
-// $(document).keyup(function(e) {
-//     console.log(e.keyCode);
-//
-//     var reset = function(e) {
-//         var start = false;
-//         return alert("Try Again Buckaroo!");
-//     }
-//     //keyCode MAPS OUT WHAT KEY THE USER IS PRESSING BASED ON THEIR NUMERIC VALUE. IF WE LOOK AT THE VAR konami, THE NUMBERS REPRESENT WHAT KEYS I WANT THE USER TO INPUT. THE CODE BELOW TAKES IN MY EMPTY ARRAY AND WILL PUSH WHAT KEYS THE USER PRESSES BASED ON THEIR NUMERIC VALUE.
-//
-//    keys.push(e.keyCode);
-//
-//     if(!start) {
-//         if(keys === 38) {
-//             return alert("yay!")
-//         } else {
-//             return reset;
-//         }
-//     }
-//
-//     //     && keys[0] === konami[0]) {
-//     //     konami++;
-//     // } else {
-//     //     reset();
-//     // }
-//
-//
-// });
